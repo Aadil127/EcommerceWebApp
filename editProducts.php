90f567afc5cadf633ce21c3db0ebdf095940338a
Aadil127 <aadilsaiyed230405@gmail.com>
2025-10-24 15:52:07 +0530

    Added feature to remove product

--- editProducts.php
+++ editProducts.php
@@ -21,7 +21,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         $fileSize = $_FILES["image"]["size"];
         $fileType = $_FILES["image"]["type"];
         $productName = $_POST["productName"];
-        $catagory = $_POST["catagory"];
+        $category = $_POST["category"];
         $quatity = $_POST["quatity"];
         $pruchasedPrice = $_POST["pruchasedPrice"];
         $price = $_POST["price"];
@@ -43,19 +43,41 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
             // echo "success";
 
             $statement = $conn->prepare("INSERT INTO products(Name, Price, ImgPath, Quantity, Category, PurchasedPrice, StartingQuantity) VALUES (?, ?, ?, ?, ?, ?, ?)");
-            $statement->bind_param("sisisii", $productName,$price, $destination, $quatity, $catagory, $pruchasedPrice, $quatity);
+            $statement->bind_param("sisisii", $productName,$price, $destination, $quatity, $category, $pruchasedPrice, $quatity);
             if($statement->execute()){
                 ?><script>alert("Sucessfuly added product")</script><?php
 
                 header("Location: " . $_SERVER["PHP_SELF"] . "?success=1");
                 exit();
             }
+            $statement->close();
         }
         else 
             echo "Error: Could not move file to uploads folder.";
     }
     else
         echo "Error: " . $_FILES['image']['error'];
+}
+
+if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET["productID"])){
+    $productName = $_GET["productName"];
+    $productId = $_GET["productId"];
+
+    $statement = $conn->prepare("SELECT ImgPath FROM products WHERE ID = ? AND Name = ?");
+    $statement->bind_param("is", $productId, $productName);
+    $statement->execute();
+
+    $result = $statement->get_result();
+    $row = $result->fetch_assoc();
+    if (is_file($row["ImgPath"])) {
+        unlink($row["ImgPath"]);
+    }
+
+
+    $statement = $conn->prepare("DELETE FROM products WHERE ID = ? AND Name = ?");
+    $statement->bind_param("is", $productId, $productName);
+    $statement->execute();
+    $statement->close();
 }
 
 $conn->close();
@@ -68,8 +90,9 @@ window.addEventListener("load", function() {
 </script>
 
 <form action="editProducts.php" method="POST" enctype="multipart/form-data">
+    <h3>Add New Product</h3>
     <input type="text" name="productName" placeholder="Name of product" required>
-    <input type="text" name="catagory" placeholder="Catagory of product" required>
+    <input type="text" name="category" placeholder="Category of product" required>
     <input type="number" name="quatity" placeholder="product quatity" required>
     <input type="number" name="pruchasedPrice" placeholder="purchase price of product" required>
     <input type="number" name="price" placeholder="Selling price of product" required>
@@ -79,6 +102,14 @@ window.addEventListener("load", function() {
         <label for="imageInput" id="imageLabel">Choose image</label>
     </div>
     <button type="submit">Upload</button>
+</form>
+
+
+<form action="editProducts.php" method="GET">
+    <h3>Remove Product</h3>
+    <input type="number" name="productId" placeholder="Enter the product Id" required>
+    <input type="text" name="productName" placeholder="Enter the product Name" required>
+    <button type="submit">Submit</button>
 </form>
 
 <?php
